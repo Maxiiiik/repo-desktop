f107e389e02339fc8633fd30465bf97c8403bd0c
Maxi <shuprudkomax00@gmail.com>
2021-03-02 14:06:46 +0200

    [2.3] add "-c" in numwrds

--- numwrds.cpp
+++ numwrds.cpp
@@ -7,13 +7,27 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	char arr[100];
+	int spacecounter = 1;
 	for (size_t i = 0; i < argc; i++)
 	{
-
+		if (strcmp(argv[i], "-c") == 0)
+		{
+			strcpy_s(arr, argv[i + 1]);
+			for (size_t j = i + 2; j < argc; j++)
+			{
+				spacecounter++;
+				strcat_s(arr, " ");
+				strcat_s(arr, argv[j]);
+			}
+			cout << "str: " << arr << endl;
+			cout << "Number of words: " << spacecounter << endl;
+			return 0;
+		}
 		if (strcmp(argv[i], "-h") == 0)
 		{
 			cout << "This program WORK" << endl;
-				return 0;
+			return 0;
 		}
 	}
     string str;
